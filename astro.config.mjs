d14ae0ea03aed773e49d638abfbad0f698679e87
Abdou Aziz Ndao <azizndao71@gmail.com>
2023-06-25 00:48:12 +0000

    Adding dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import prefetch from "@astrojs/prefetch";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), mdx(), prefetch(), react()]
+});
