92f4933f88138828f60fb3dc21aea702c42a02bd
Abdou Aziz Ndao <azizndao71@gmail.com>
2023-06-26 23:57:48 +0000

    Adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,37 @@
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [image(), mdx({
-    drafts: true
-  }), prefetch(), react(), tailwind()],
+  integrations: [
+    image(),
+    mdx({
+      drafts: true,
+    }),
+    prefetch(),
+    react(),
+    tailwind(),
+  ],
   markdown: {
     drafts: true,
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: "append"
-    }]]
-  }
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+        },
+      ],
+    ],
+  },
+  adapter: vercel(),
 });
