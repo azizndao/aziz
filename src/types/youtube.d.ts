30a92efb2a3e6533289af720c1782ccfb12cc5da
Abdou Aziz Ndao <azizndao71@gmail.com>
2023-07-04 01:07:17 +0000

    Deleting unnecessary files and display mode to 'standalone"

--- src/types/youtube.d.ts
+++ src/types/youtube.d.ts
@@ -1,68 +1,68 @@
 export interface YoutubePageInfo {
-  totalResults: number;
-  resultsPerPage: number;
+  totalResults: number
+  resultsPerPage: number
 }
 
 export interface Thumbnail {
-  url: string;
-  width: number;
-  height: number;
+  url: string
+  width: number
+  height: number
 }
 
-export type AllThumbnails = Record<"default" | "medium" | "high", Thumbnail>;
+export type AllThumbnails = Record<'default' | 'medium' | 'high', Thumbnail>
 
 export interface Playlist {
-  kind: string;
-  etag: string;
-  id: string;
+  kind: string
+  etag: string
+  id: string
   snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: AllThumbnails;
-  };
+    publishedAt: string
+    channelId: string
+    title: string
+    description: string
+    thumbnails: AllThumbnails
+  }
   contentDetails: {
-    itemCount: number;
-  };
+    itemCount: number
+  }
 }
 
 interface Video {
-  kind: "youtube#playlistItem";
-  etag: string;
-  id: string;
+  kind: 'youtube#playlistItem'
+  etag: string
+  id: string
   snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: AllThumbnails;
-    channelTitle: string;
-    videoOwnerChannelTitle: string;
-    videoOwnerChannelId: string;
-    playlistId: string;
-    position: number;
+    publishedAt: string
+    channelId: string
+    title: string
+    description: string
+    thumbnails: AllThumbnails
+    channelTitle: string
+    videoOwnerChannelTitle: string
+    videoOwnerChannelId: string
+    playlistId: string
+    position: number
     resourceId: {
-      kind: string;
-      videoId: string;
-    };
-  };
+      kind: string
+      videoId: string
+    }
+  }
   contentDetails: {
-    videoId: string;
-    startAt: string;
-    endAt: string;
-    note: string;
-    videoPublishedAt: string;
-  };
+    videoId: string
+    startAt: string
+    endAt: string
+    note: string
+    videoPublishedAt: string
+  }
   status: {
-    privacyStatus: string;
-  };
+    privacyStatus: string
+  }
 }
 
 export interface YoutubeResponse<T> {
-  pageInfo: YoutubePageInfo;
-  items: T[];
+  pageInfo: YoutubePageInfo
+  items: T[]
   contentDetails: {
-    itemCount: number;
-  };
+    itemCount: number
+  }
 }
