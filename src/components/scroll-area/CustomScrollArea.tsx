612b1a34d6bb5f265f3787896e22a0e171e6f4ee
Abdou Aziz Ndao <azizndao71@gmail.com>
2023-07-04 10:52:52 +0000

    Changing tab size to 4

--- src/components/scroll-area/CustomScrollArea.tsx
+++ src/components/scroll-area/CustomScrollArea.tsx
@@ -1,35 +1,35 @@
-import * as ScrollArea from "@radix-ui/react-scroll-area";
-import type { PropsWithChildren } from "react";
-import { twMerge } from "tailwind-merge";
-import styles from "./CustomScrollArea.module.css";
+import * as ScrollArea from "@radix-ui/react-scroll-area"
+import type { PropsWithChildren } from "react"
+import { twMerge } from "tailwind-merge"
+import styles from "./CustomScrollArea.module.css"
 
 type Props = PropsWithChildren<{
-  className?: string;
-}>;
+    className?: string
+}>
 
 export default function CustomScrollArea({ children, className }: Props) {
-  return (
-    <ScrollArea.Root
-      className={twMerge(styles.ScrollAreaRoot, className)}
-      scrollHideDelay={2000}
-    >
-      <ScrollArea.Viewport className={styles.ScrollAreaViewport}>
-        {children}
-      </ScrollArea.Viewport>
+    return (
+        <ScrollArea.Root
+            className={twMerge(styles.ScrollAreaRoot, className)}
+            scrollHideDelay={2000}
+        >
+            <ScrollArea.Viewport className={styles.ScrollAreaViewport}>
+                {children}
+            </ScrollArea.Viewport>
 
-      <ScrollArea.Scrollbar
-        className={styles.ScrollAreaScrollbar}
-        orientation="vertical"
-      >
-        <ScrollArea.Thumb className={styles.ScrollAreaThumb} />
-      </ScrollArea.Scrollbar>
-      <ScrollArea.Scrollbar
-        className={styles.ScrollAreaScrollbar}
-        orientation="horizontal"
-      >
-        <ScrollArea.Thumb className={styles.ScrollAreaThumb} />
-      </ScrollArea.Scrollbar>
-      <ScrollArea.Corner className={styles.ScrollAreaCorner} />
-    </ScrollArea.Root>
-  );
+            <ScrollArea.Scrollbar
+                className={styles.ScrollAreaScrollbar}
+                orientation="vertical"
+            >
+                <ScrollArea.Thumb className={styles.ScrollAreaThumb} />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Scrollbar
+                className={styles.ScrollAreaScrollbar}
+                orientation="horizontal"
+            >
+                <ScrollArea.Thumb className={styles.ScrollAreaThumb} />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner className={styles.ScrollAreaCorner} />
+        </ScrollArea.Root>
+    )
 }
