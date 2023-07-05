f0baefbae70db7ed022c5976d314f51a04a756a0
Abdou Aziz Ndao <azizndao71@gmail.com>
2023-07-05 23:57:17 +0000

    Change the code background on dark mode

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,44 +1,44 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from "astro:content"
 
 export const baseSchema = z
-  .object({
-    title: z.string(),
-    description: z.string(),
-    type: z.literal('base').optional().default('base'),
-    tags: z.array(z.string()).optional().default([]),
-    date: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    index: z.number().optional(),
-    i18nReady: z.boolean().default(false),
-    draft: z.boolean().default(false),
-  })
-  .strict()
+    .object({
+        title: z.string(),
+        description: z.string(),
+        type: z.literal("base").optional().default("base"),
+        tags: z.array(z.string()).optional().default([]),
+        date: z
+            .string()
+            .or(z.date())
+            .transform((val) => new Date(val)),
+        updatedDate: z
+            .string()
+            .optional()
+            .transform((str) => (str ? new Date(str) : undefined)),
+        index: z.number().optional(),
+        i18nReady: z.boolean().default(false),
+        draft: z.boolean().default(false)
+    })
+    .strict()
 
 const blog = defineCollection({
-  schema: baseSchema.extend({
-    type: z.literal('blog').optional().default('blog'),
-    cover: z.string().optional(),
-    author: z.string().optional().default('Abdou Aziz Ndao'),
-    category: z.string(),
-  }),
+    schema: baseSchema.extend({
+        type: z.literal("blog").optional().default("blog"),
+        cover: z.string().optional(),
+        author: z.string().optional().default("Abdou Aziz Ndao"),
+        category: z.string()
+    })
 })
 
 export const pythonSchema = baseSchema.extend({
-  type: z.literal('python'),
+    type: z.literal("python")
 })
 
 export const flutterSchema = baseSchema.extend({
-  type: z.literal('flutter'),
+    type: z.literal("flutter")
 })
 
 const courses = defineCollection({
-  schema: z.union([pythonSchema, flutterSchema]),
+    schema: z.union([pythonSchema, flutterSchema])
 })
 
 export const collections = { courses, blog }
